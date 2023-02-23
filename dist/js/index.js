34b04b49ecb3fabfd24291175aac3819ff57d56e
Camila <camillacab@hotmail.com>
2023-02-23 15:44:30 -0500

    add form submission

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,9 +1,11 @@
 const lines = document.querySelectorAll('.line')
-const progressBar = document.querySelectorAll('.progress')
+const progressBar = document.querySelectorAll('.percentage')
 const projectsBoxes = document.querySelectorAll('.projects-box')
 const firstProjectBox = document.querySelector('#first-project-box')
+const form = document.querySelector('#contact-form')
+const subMessage = document.querySelector('#submission-message')
 
-const isInViewport = el => {
+const isInViewport = (el) => {
   const rect = el.getBoundingClientRect()
   return (
     rect.top >= 0 &&
@@ -15,40 +17,40 @@ const isInViewport = el => {
 }
 
 const run = () => {
-  lines.forEach(line => {
+  lines.forEach((line) => {
     if (isInViewport(line)) {
       line.classList.add('line-expanded')
     }
   })
-  progressBar.forEach(bar => {
+  progressBar.forEach((bar) => {
     if (isInViewport(bar)) {
       switch (bar.id) {
         case 'css':
-          bar.style.width = '75%'
+          bar.style.width = '65%'
           break
         case 'js':
-          bar.style.width = '80%'
+          bar.style.width = '85%'
           break
         case 'node':
           bar.style.width = '75%'
           break
         case 'react':
-          bar.style.width = '50%'
-          break
-        case 'api':
           bar.style.width = '80%'
           break
-        case 'sql':
-          bar.style.width = '85%'
-          break
-        case 'java':
-          bar.style.width = '75%'
-          break
-        case 'ios':
+        case 'next':
           bar.style.width = '60%'
           break
+        case 'python':
+          bar.style.width = '60%'
+          break
+        case 'sql':
+          bar.style.width = '70%'
+          break
+        case 'java':
+          bar.style.width = '55%'
+          break
         case 'cpp':
-          bar.style.width = '65%'
+          bar.style.width = '50%'
           break
         default:
           break
@@ -68,3 +70,31 @@ const run = () => {
 window.addEventListener('load', run)
 window.addEventListener('resize', run)
 window.addEventListener('scroll', run)
+
+document.addEventListener('DOMContentLoaded', function () {
+  var gallery = document.querySelectorAll('.materialboxed')
+  M.Materialbox.init(gallery, {})
+})
+
+form.addEventListener('submit', (e) => {
+  // e.preventDefault()
+  subMessage.style.display = 'block'
+})
+
+window.scroll({
+  top: 2500,
+  left: 0,
+  behavior: 'smooth',
+})
+
+// Scroll certain amounts from current position
+window.scrollBy({
+  top: 100, // could be negative value
+  left: 0,
+  behavior: 'smooth',
+})
+
+// Scroll to a certain element
+document.querySelector('.hello section').scrollIntoView({
+  behavior: 'smooth',
+})
